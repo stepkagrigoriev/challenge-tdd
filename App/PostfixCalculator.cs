da56a4b336b629243941ad27ecc7947bb3599c19
stepangrigoryev <grigoriev.stepka@yandex.ru>
2024-11-06 19:31:18 +0500

    v0.1 (без комплов и не заходит один тест)

--- App/PostfixCalculator.cs
+++ App/PostfixCalculator.cs
@@ -1,14 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Linq.Expressions; 
+using System.Runtime.CompilerServices; 
+ 
+namespace App 
+{ 
+    public static class PostfixCalculator 
+    { 
+        public static string Calculate(string postfixExpression) 
+        { 
+            if (postfixExpression == null) 
+                throw new FormatException(); 
+            if (postfixExpression.Length == 0) 
+                return "0"; 
+            var array = postfixExpression.Split(' ').ToList(); 
+            int a,b; 
+            string c; 
+            while (array.Count() > 1) 
+            {
+                try
+                {
+                    c = array[2];
+                }
+                catch
+                {
+                    throw new FormatException();
+                }
 
-namespace App
-{
-    public static class PostfixCalculator
-    {
-        public static string Calculate(string postfixExpression)
-        {
-            throw new NotImplementedException();
-        }
-    }
+                if (int.TryParse(array[0], out a) && int.TryParse(array[1], out b)) 
+                { 
+                    array[0] = NewResult(a, b, c).ToString(); 
+                    array.RemoveAt(1);
+                    array.RemoveAt(1);
+                } 
+                else
+                    throw new FormatException(); 
+            } 
+            if (!int.TryParse(array[0], out a)) 
+                throw new FormatException(); 
+            return array[0]; 
+        } 
+        public static double NewResult(double a, double b, string c) 
+        { 
+            if (c == "+") 
+                return a + b; 
+            if (c == "-") 
+                return a - b; 
+            if (c == "*") 
+                return a * b; 
+            throw new FormatException(); 
+        } 
+    } 
 }
